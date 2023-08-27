c734503c9357310176464787f7ba9dda20a7376d
QuynhLa98 <quynhla98@gmail.com>
2023-08-27 20:44:51 +0700

    done logic

--- buoi3/src/components/BaiTapDatVe/ChiTiet.jsx
+++ buoi3/src/components/BaiTapDatVe/ChiTiet.jsx
@@ -3,11 +3,24 @@ import { connect } from "react-redux";
 import { datGheAction } from "../../store/actions/datGheAction";
 
 class ChiTiet extends Component {
+  calculateTotalAmount = () => {
+    let total = 0;
+    this.props.danhSachGhe.forEach((element) => {
+      element.danhSachGhe.forEach((ghe) => {
+        if (ghe.dangChon) {
+          total += ghe.gia;
+        }
+      });
+    });
+    return total;
+  };
+
   renderContent = () => {
     return this.props.danhSachGhe.map((element) => {
       return this.renderChiTiet(element.danhSachGhe, element.hang);
     });
   };
+
   renderChiTiet = (data, hang) => {
     const formatNum = new Intl.NumberFormat("VN-vn");
     return data.map((element) => {
@@ -15,11 +28,11 @@ class ChiTiet extends Component {
       if (dangChon) {
         return (
           <p key={soGhe}>
-            Ghế: {soGhe} - { formatNum.format(gia)} VND
+            Ghế: {soGhe} - {formatNum.format(gia)} VND
             <span
               onClick={() => {
                 this.props.dispatch(datGheAction(element, hang));
-              }} 
+              }}
               className="ml-3 cursor-pointer text-danger"
             >
               [HỦY]
@@ -29,14 +42,11 @@ class ChiTiet extends Component {
       }
     });
   };
-  tongGheDat = ()=>{
-    const data = this.props.danhSachGhe.map((element)=>{return element.danhSachGhe.filter((element)=>{return element.dangChon})})
-    console.log(data);
-  }
-  tongTien = () => {
-   
-  };
+
   render() {
+    const formatNum = new Intl.NumberFormat("VN-vn");
+    const totalAmount = this.calculateTotalAmount();
+
     return (
       <div className="col-2">
         <button className="btn btn-success">Ghe Dang Chon</button>
@@ -44,17 +54,21 @@ class ChiTiet extends Component {
           Ghe Da Dat
         </button>
         <button className="btn btn-light">Ghe Chua Dat</button>
-        <h5 className="text-warning">Danh sách ghế đã đặt ({this.props.danhSachGhe.length})</h5>
+        <h5 className="text-warning">
+          Danh sách ghế đã đặt ({this.props.danhSachGhe.length})
+        </h5>
         {this.renderContent()}
-        {this.tongGheDat()}
-        
+        <p>Tổng tiền: {formatNum.format(totalAmount)} VND</p>
       </div>
     );
   }
 }
+
 const mapStateToProps = (state) => {
   return {
     danhSachGhe: state.datVeReducer.danhSachGhe,
   };
 };
+
 export default connect(mapStateToProps)(ChiTiet);
+
